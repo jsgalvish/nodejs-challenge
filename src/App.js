5f71be0c0cbeb4ea2389e41850be4af27c307659
JS Galvis H <jsgalvish@unal.edu.co>
2020-01-25 15:20:23 -0500

    Chat Input Behavior

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component, Fragment} from 'react';
 import ChatContainer from './components/ChatContainer';
 import './css/App.css';
 
-//const io = require('socket.io-client');
+const io = require('socket.io-client');
 
 //let socket = io('http://localhost:5000');
 //socket.emit('newMessage', 'Hi There!');
@@ -11,6 +11,15 @@ class App extends Component {
 
   constructor(props) {
     super(props);
+    this.state = { url: 'http://localhost:5000' }
+  }
+
+  initSocket(){
+    this.io= io(this.state.url)
+  }
+
+  componentDidMount() {
+    this.initSocket()
   }
 
   render(){
--- src/components/ChatInputBar.js
+++ src/components/ChatInputBar.js
@@ -5,12 +5,22 @@ class ChatInputBar extends Component{
     super(props);
   }
 
+  sendMessage(e){
+    e.preventDefault()
+    console.log(`New Message:  ${this.msgInput.value}`)
+  }
+
   render(){
     return(
       <div className='chat-bar'>
         <form id='chat-form'>
-          <input id='chat-input' type='text' placeholder='Type your message...'/>
-          <button id='chat-submit' type='submit'>Send Message</button>
+          <input
+            id='chat-input'
+            type='text'
+            placeholder='Type your message...'
+            ref={(input) => {this.msgInput = input}}
+          />
+          <button id='chat-submit' type='submit' onClick={this.sendMessage.bind(this)}>Send Message</button>
         </form>
       </div>
     )
